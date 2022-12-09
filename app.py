4b242f84cb28adc135bfb273b05ce5267a3de98d
sasa <n-erk.house@outlook.com>
2022-12-09 15:32:51 +0900

    一時保存３

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask import render_template
-from flask import redirect
+from flask import redirect,request
 from flask import session
 import sqlite3
 import datetime
@@ -14,23 +14,38 @@ def test_index():
 
 # 投稿機能（sasaさん担当）
 @app.route("/add")
-def add_get():
+def add():
     return render_template("add.html")
 
 @app.route("/add",methods=["POST"])
 def add_post():
-    py_space_list = reguest.form.get("html_")
-    conn =squlite3.connect("bk.db")
+    
+    py_space = request.form.get("space")
+    py_date = request.form.get("date")
+    py_start_time = request.form.get("start_time")
+    py_end_time = request.form.get("end_time")
+    py_capacity = request.form.get("capacity")
+    py_price = request.form.get("price")
+    py_memo = request.form.get("memo")
+    
+    
+
+    conn =sqlite3.connect("bk.db")
+
     c=conn.cursor()
-    c.execute("INSERT INTO task values(null,?)",(py_task,))
+    c.execute("INSERT INTO space_list values(null,?,?,?,?,?,?,?,0,null)",(py_space,py_date,py_start_time,py_end_time,py_capacity,py_price,py_memo))
+    createAt1= datetime.date.today()
+    createAt="{0:%y/%m/%d}".format(createAt1)
     conn.commit()
+  
+
     c.close()
     return redirect("/index")
 
 
 # 一覧・検索機能（Takkaさん担当）
-@app.route("/list/")
-def bk_list():
+# @app.route("/list/")
+# def bk_list():
     #データベースつくってそこにアクセスする。
 
 
